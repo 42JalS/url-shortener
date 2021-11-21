33bd392ebb1ce049a1c2c09f3e3491521c08d2f4
EeeUnS <euns312510@gmail.com>
2021-11-21 16:42:52 +0900

    🐛 FIX: apply linter to files

--- src/bijective.js
+++ src/bijective.js
@@ -1,24 +1,26 @@
-const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 const base = alphabet.length;
 
-module.exports.encode = (num) => {
-    var encoded = '';
-    if (num == 0) return alphabet[num];
-    while (num) {
-        var remainder = num % base;
-        num = Math.floor(num / base);
-        encoded = alphabet[remainder].toString() + encoded;
-    }
-    return encoded;
+module.exports.encode = (inputNum) => {
+  let encoded = '';
+  let num = inputNum;
+  if (num === 0) return alphabet[num];
+  while (num) {
+    const remainder = num % base;
+    num = Math.floor(num / base);
+    encoded = alphabet[remainder].toString() + encoded;
+  }
+  return encoded;
 };
 
-module.exports.decode = (str) => {
-    var decoded = 0;
-    while (str) {
-        var index = alphabet.indexOf(str[0]);
-        var power = str.length - 1;
-        decoded += index * (Math.pow(base, power));
-        str = str.substring(1);
-    }
-    return decoded;
+module.exports.decode = (intpuStr) => {
+  let str = inputStr;
+  let decoded = 0;
+  while (str) {
+    const index = alphabet.indexOf(str[0]);
+    const power = str.length - 1;
+    decoded += index * (base ** power);
+    str = str.substring(1);
+  }
+  return decoded;
 };

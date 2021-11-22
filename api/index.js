4edf1603f33e1655f382fd502d2909acb95a6f48
srngch <srngch.dev@gmail.com>
2021-11-22 13:59:23 +0900

    WIP

--- api/index.js
+++ api/index.js
@@ -1,1 +1,37 @@
 const bijective = require('../services/bijective');
+const Urls = require('../models/urls');
+
+/// ////////
+app.get('/url/:longUrl', async (req, res) => {
+  console.log(`get: /url/${req.params.longUrl}`);
+
+  try {
+    const doc = await Urls.findOne({ url: req.params.longUrl });
+    if (doc) {
+      res.send({
+        host: `${req.hostname}:${SERVER_PORT}`,
+        key: bijective.encode(doc._id),
+      });
+    } else {
+      const newUrl = await Urls.create({ url: req.params.longUrl });
+      res.send({
+        host: `${req.hostname}:${SERVER_PORT}`,
+        key: bijective.encode(newUrl._id),
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get('/:key', (req, res) => {
+  console.log(`get: /${req.params.key}`);
+  const id = bijective.decode(req.params.key);
+  console.log(`id: ${id}`);
+  Urls.findById(id, (err, doc) => {
+    if (err) return res.send(err);
+    if (doc) return res.redirect(doc.url);
+    res.redirect('/');
+  });
+});
+/// ////////

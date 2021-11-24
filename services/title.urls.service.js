58501d67ec14965a98ba7f3f88f268d6ceaa2cc8
EeeUnS <euns312510@gmail.com>
2021-11-24 21:11:06 +0900

    🐛 FIX: 특수문자 url 제대로 링크안해주는 부분

--- services/title.urls.service.js
+++ services/title.urls.service.js
@@ -2,19 +2,26 @@ const Urls = require('../models/urls');
 const metadata = require('../utils/metadata');
 const urlService = require('./urls.service');
 
-
+const removeSpecificType= (str)=> {
+    const reg = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/ ]/gim;
+    if(reg.test(str)){
+      return str.replace(reg, "-");    
+    } 
+      return str;
+      
+  }
 const getConvertedTitleUrl = async (originalUrl) =>{
     console.log("getTitleConvertedUrl");
     const title = await metadata.getTitle(originalUrl);
 
     console.log(title);
-    
-    // if null
-    // if(title === undefined){
-    //     return title;
-    // }
+    //error check
+    // title이 겹치는데 다른 url인 경우.
+    // custom부분 과 겹치는 title인 경우.?
 
-    const convertedUrl = await urlService.getConvertedUrl(originalUrl, title);
+    const filteredTitle = removeSpecificType(title);
+    console.log(title);
+    const convertedUrl = await urlService.getConvertedUrl(originalUrl, filteredTitle);
 
     console.log(convertedUrl);
     return convertedUrl;

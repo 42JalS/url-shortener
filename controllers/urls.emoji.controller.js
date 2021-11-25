87e45361cb78fe70cf5f647bc1e8cec8e14a6ff2
Chaejin-jen <jen71324@gmail.com>
2021-11-25 21:01:47 +0900

    ♻️REFACTOR: change method to post

--- controllers/urls.emoji.controller.js
+++ controllers/urls.emoji.controller.js
@@ -4,13 +4,8 @@ const service = require('../services/urls.emoji.service');
 exports.changeOrignalUrlToConvertedEmojiUrl = async (req, res, next) => {
   console.log('changeOrignalUrlToConvertedEmojiUrl');
   try {
-<<<<<<< HEAD
     const { originalUrl } = req.params;
     const convertedEmojiUrl = await service.getConvertedEmojiUrlOrNULL(originalUrl);
-=======
-    const { emojiUrl } = req.body;
-    const convertedEmojiUrl = await service.getConvertedEmojiUrl(emojiUrl);
->>>>>>> 6d8e8fb (♻️REFACTOR: change method to post)
     res.status(httpStatus.OK).send({
       key: convertedEmojiUrl,
     });
@@ -19,3 +14,4 @@ exports.changeOrignalUrlToConvertedEmojiUrl = async (req, res, next) => {
     next(err);
   }
 };
+

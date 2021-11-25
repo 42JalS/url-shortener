f79e03467c42396a2841c4514df87a5fe91b5731
EeeUnS <euns312510@gmail.com>
2021-11-25 21:11:54 +0900

    📦 NEW: add isResponseHttpCodeOK

--- utils/metadata.js
+++ utils/metadata.js
@@ -27,12 +27,22 @@ exports.getTitleOrNULL = async (originalUrl) => {
     return title;
 }
 
-exports.getMetaDataList = async (originalUrl) => {
-    const res = await axios.get(originalUrl);
-    const {data} = res;
-    console.log(data);
-    const $ = cheerio.load(data);
-    // metadata 처리하는 모듈에서 사용함
-    // 이부분 앞으로 구현할 부분
-};
+exports.isResponseHttpCodeOK = async (url) => {
+    const res = await axios.get(url).catch((error) => {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            } else if (error.request) {
+            console.log(error.request);
+            } else {
+            console.log('Error', error.message);
+            }
+            console.log(error.config);
+        }
+    );
+    console.log(res);
+    return res !== undefined
+}
+
 

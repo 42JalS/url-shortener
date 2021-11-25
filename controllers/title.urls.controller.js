3c400af415548f9c1b2a18b5d5c9aa590646250a
EeeUnS <euns312510@gmail.com>
2021-11-25 19:26:46 +0900

    🐛 FIX: 처리할수없는 경우 에러처리

--- controllers/title.urls.controller.js
+++ controllers/title.urls.controller.js
@@ -7,9 +7,14 @@ exports.changeOriginalUrlToTitleConvertedUrl = async (req, res, next) => {
     const { originalUrl } = req.params;
     console.log(originalUrl);
 
-    const convertedUrl = await service.getTitleConvertedUrl(originalUrl);
+    const convertedUrl = await service.getTitleConvertedUrlOrNULL(originalUrl);
     console.log(convertedUrl);
-    res.status(httpStatus.OK).send({
+    if(convertedUrl === null){
+        return res.status(httpStatus.BAD_REQUEST).send({
+          message: 'Reject get title-url some reason',
+      });
+    }
+    return res.status(httpStatus.OK).send({
       key: convertedUrl,
     });
   } catch (err) {

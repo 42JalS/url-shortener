1e73dd8b3adf7dcc67851a93f65da8bcd02cf696
m1nje <rkdalswp111@naver.com>
2021-11-26 15:32:21 +0900

    📦 NEW: validate url

---
+++ utils/validate.js
@@ -0,0 +1,11 @@
+exports.isValidCustomUrl = customUrl => {
+	if (customUrl === '' || !customUrl) return false;
+	const regex = /\p{P}|\p{C}|\p{Z}|\s/gu;
+	return !regex.test(url.replace(/[-_]/g, ''));
+}
+
+exports.isValidCustomUrlisValidOriginalUrl = originalUrl => {
+	if (originalUrl === '' || !originalUrl) return false;
+	const regex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+	return regex.test(originalUrl);
+}

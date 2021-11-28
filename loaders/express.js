ac5937503ad6bd432a8369bc4f6863d5a03ad173
srngch <srngch.dev@gmail.com>
2021-11-28 17:36:19 +0900

    📦 NEW: add cors(all)

--- loaders/express.js
+++ loaders/express.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const morgan = require('morgan');
 const logger = require('./winston');
 const env = require('../config');
@@ -19,6 +20,7 @@ module.exports = async app => {
     res.status(200).end();
   });
 
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(express.static(path.join(__dirname, 'public')));
